695133f851cd99f1810472765b699d3778fa4294
Royce119 <madcoder911@gmail.com>
2023-09-04 23:35:47 +0300

    finished navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
-  return <h1 className="text-3xl underline font-bold p-3">Hey</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
